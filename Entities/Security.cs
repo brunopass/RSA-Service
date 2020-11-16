64c61d222a339b48f08d639c039dceaf56a6c538
Bruno <brunoppass@gmail.com>
2020-11-16 14:01:30 -0300

    master

---
+++ Entities/Security.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Xml;
+
+namespace Entities
+{
+    public class Security
+    {
+        private string privateKey;
+        private string publicKey;
+        private static UnicodeEncoding _encoder = new UnicodeEncoding();
+
+        public Security()
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            privateKey = rsa.ToXmlString(true);
+            publicKey = rsa.ToXmlString(false);
+        }
+
+        private string Decrypt(string data)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            var dataArray = data.Split(new char[] { ',' });
+            var dataByte = new byte[dataArray.Length];
+
+            for (int i = 0; i < dataArray.Length; i++)
+            {
+                dataByte[i] = Convert.ToByte(dataArray[i]);
+            }
+
+            rsa.FromXmlString(privateKey);
+            var decryptedByte = rsa.Decrypt(dataByte, false);
+            return _encoder.GetString(decryptedByte);
+        }
+
+        private string Encrypt(string data)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(publicKey);
+            byte[] dataToEncrypt = _encoder.GetBytes(data);
+            byte[] encryptedByteArray = rsa.Encrypt(dataToEncrypt, false).ToArray();
+            int length = encryptedByteArray.Count();
+            int item = 0;
+            StringBuilder sb = new StringBuilder();
+            foreach (byte x in encryptedByteArray)
+            {
+                item++;
+                sb.Append(x);
+
+                if (item < length)
+                    sb.Append(",");
+            }
+
+            return sb.ToString();
+        }
+
+        public string GetPublicKey()
+        {
+            return publicKey;
+        }
+
+        public string Encryptation(string filePath)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(filePath);
+            XmlNode node = xmlDocument.DocumentElement.SelectSingleNode("/file/type");
+
+            if(node.InnerText == "decrypt")
+            {
+                try
+                {
+                    return Decrypt(
+                        xmlDocument.DocumentElement.SelectSingleNode("/file/text").InnerText
+                    ) ;
+                }
+                catch(Exception error)
+                {
+                    throw new Exception(error.Message);
+                }
+            }
+
+            try
+            {
+                return Encrypt(
+                        xmlDocument.DocumentElement.SelectSingleNode("/file/text").InnerText
+                    );
+            }
+            catch (Exception error)
+            {
+                throw new Exception(error.Message);
+            }
+        }
+    }
+}
---
+++ PARCIAL2PASSARELLIBRUNO/Service1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using System.IO;
+
+namespace PARCIAL2PASSARELLIBRUNO
+{
+    public partial class AuthService : ServiceBase
+    {
+        private Security security = new Security();
+        private string path = "C:/Users/bruno/Desktop/rsa";
+
+        public AuthService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer1.Start();
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                Write(security.GetPublicKey(), "/public.xml");
+                Read("/file.xml");
+            }
+            catch(Exception error)
+            {
+                Write(error.Message, "/error.txt");
+            }
+        }
+
+        private void Write(string Message, string file)
+        {
+            try
+            {
+                if (!Directory.Exists(this.path))
+                {
+                    Directory.CreateDirectory(this.path);
+                }
+
+                string filepath = this.path + file;
+                if (!File.Exists(filepath))
+                {
+                    using (StreamWriter streamWriter = File.CreateText(filepath))
+                    {
+                        streamWriter.WriteLine(Message);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter streamWriter = File.CreateText(filepath))
+                    {
+                        streamWriter.WriteLine(Message);
+                    }
+                }
+            }
+            catch(Exception error)
+            {
+                throw new Exception(error.Message);
+            }
+        }
+
+        private void Read(string file)
+        {
+            string filepath = this.path + file;
+            if (!File.Exists(filepath))
+            {
+                // todavia no se envio ningun archivo para desencriptar
+            }
+            else
+            {   
+
+                try
+                {
+                    Write(security.Encryptation(filepath), "/crypto.txt");
+                    File.Delete(filepath);
+                }
+                catch (Exception error)
+                {
+                    throw new Exception(error.Message);
+                }
+            }
+        }
+    }
+}
